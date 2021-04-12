2a7685158b99d0adede69e9caca01d3d5700384c
Phillip Jensen <phillip@golemgrid.com>
2021-04-12 14:15:13 +0200

    Query node endpoint

--- stats-backend/api/views.py
+++ stats-backend/api/views.py
@@ -24,6 +24,19 @@ def online_nodes(request):
 
 
 @api_view(['GET', ])
+def node(request, node_id):
+    """
+    Retrieves data about a specific node.
+    """
+    if request.method == 'GET':
+        data = Node.objects.filter(node_id=node_id)
+        serializer = NodeSerializer(data)
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', ])
 def general_stats(request):
     """
     List network stats for online nodes.
