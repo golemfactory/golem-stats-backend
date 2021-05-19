62c371c350d4737c30b16cc7dc52aacd4f606b05
Phillip Jensen <phillip@golemgrid.com>
2021-05-19 14:11:20 +0200

    Market db cleanup 10 min

--- stats-backend/start.sh
+++ stats-backend/start.sh
@@ -3,7 +3,7 @@
 mkdir -p /golem/work
 touch /golem/work/yagna.log
 echo "Starting Yagna"
-RUST_LOG=error /root/.local/bin/yagna service run > /dev/null 2>&1 &
+RUST_LOG=error MARKET_DB_CLEANUP_INTERVAL=10min /root/.local/bin/yagna service run > /dev/null 2>&1 &
 sleep 5
 key=$(/root/.local/bin/yagna app-key create requester)
 #export YAGNA_APPKEY="$(yagna app-key list --json | jq -r '.values | map(select(.[0] == "checker")) | .[0][1]')" && npm run ts:low -- --subnet-tag public-beta
