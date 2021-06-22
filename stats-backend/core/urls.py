1e7430ec768d39cc4c8d28d14637c29fa46b1773
Phillip Jensen <phillip@golemgrid.com>
2021-06-22 15:05:08 +0200

    Update urls.py

--- stats-backend/core/urls.py
+++ stats-backend/core/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('notsosecret/', admin.site.urls),
     path('v1/', include('api.urls')),
 ]
