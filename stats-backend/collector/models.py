65e05e5210b7df5fd9605110b5e6d7d2cad00a62
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2021-10-13 13:31:03 +0200

    Store info on when benchmark happened

--- stats-backend/collector/models.py
+++ stats-backend/collector/models.py
@@ -15,6 +15,7 @@ class Node(models.Model):
     updated_at = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     benchmark_score = models.IntegerField(default=0)
+    benchmarked_at = models.DateTimeField(null=True, blank=True)
 
 
 class Network(models.Model):
