8de0ba6442bb21be3227d70eb58bd68051f2cf4a
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2021-11-03 10:47:59 +0100

    Collect devnet-beta subnet nodes

--- stats-backend/yapapi/examples/low-level-api/list-offers.py
+++ stats-backend/yapapi/examples/low-level-api/list-offers.py
@@ -88,6 +88,18 @@ def main():
         )
     except TimeoutError:
         pass
+    try:
+        asyncio.get_event_loop().run_until_complete(
+            asyncio.wait_for(
+                list_offers(
+                    Configuration(),
+                    subnet_tag="devnet-beta",
+                ),
+                timeout=4,
+            )
+        )
+    except TimeoutError:
+        pass
     serialized = json.dumps(test)
     if len(test) > 450:
         r = redis.Redis(host='redis', port=6379, db=0)
