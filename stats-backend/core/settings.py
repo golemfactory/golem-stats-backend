4903fcf1a1a20dd93c9d887ce042835c453be6cb
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2022-05-06 13:04:46 +0200

    Set connection max age
    Attempt to save resources

--- stats-backend/core/settings.py
+++ stats-backend/core/settings.py
@@ -111,6 +111,7 @@ DATABASES = {
         'PASSWORD': os.environ.get("DB_PASSWORD"),
         'HOST': os.environ.get("DB_HOST"),
         'PORT': os.environ.get("DB_PORT"),
+        'CONN_MAX_AGE': 60,
     }
 }
 
