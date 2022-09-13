f5cac347bcb4ffaa0746d895a97da02190c22932
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2022-09-13 14:33:31 +0200

    #

--- stats-backend/start.sh
+++ stats-backend/start.sh
@@ -18,14 +18,10 @@
             err "invalid cputype: $_cputype"
             ;;
     esac
-# Start yagna service in the background and log it
-mkdir -p /golem/work
-touch /golem/work/yagna.log
-echo "Starting Yagna"
-/root/.local/bin/yagna service run &
-sleep 5
-key=$(/root/.local/bin/yagna app-key create requester)
-#export YAGNA_APPKEY="$(yagna app-key list --json | jq -r '.values | map(select(.[0] == "checker")) | .[0][1]')" && npm run ts:low -- --subnet-tag public-beta
-cd /stats-backend/ && cd yapapi && git checkout b0.5
 
-#key=$(/root/.local/bin/yagna app-key create requester) && export YAGNA_APPKEY=$key && npm run ts:low -- --subnet-tag public-beta
+
+echo "Starting Yagna"
+yagna service run &
+sleep 5
+key=$(yagna app-key create requester)
+cd /stats-backend/ && cd yapapi && git checkout b0.5
