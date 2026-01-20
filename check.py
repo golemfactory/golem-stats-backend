f3754cc8435f503daf61e138e1e5fe56ba7e0d5c
scx1332 <sieciech@gmail.com>
2026-01-20 19:13:16 +0100

    Add verify backup action

--- check.py
+++ check.py
@@ -1,3 +1,5 @@
+import datetime
+
 import boto3
 import lzma
 import os
@@ -9,6 +11,7 @@ BUCKET_NAME = 'golem-stats-database-backup'
 PREFIX = 'db-backups/'
 FILE_EXTENSION = '.sql.xz'
 DOWNLOAD_DIR = './downloads'
+FILE_PREFIX = 'golemstats_'
 
 def get_latest_backup_key(s3_client, bucket, prefix):
     """
@@ -49,6 +52,31 @@ def download_and_unpack():
         return
 
     filename = os.path.basename(latest_key)
+
+    #extract timestamp
+    datetime_part = filename.replace(FILE_PREFIX, '').replace(FILE_EXTENSION, '')
+
+    # 20260120_183131
+    print(f"Latest backup found: {filename} (timestamp: {datetime_part})")
+
+    dt_object = datetime.strptime(datetime_part, "%Y%m%d_%H%M%S")
+
+    print(f"Parsed timestamp: {dt_object}")
+
+    # 2. Calculate Difference
+    # Note: Use total_seconds() to get the full duration including days
+    time_diff = datetime.now() - dt_object
+    age_in_hours = time_diff.total_seconds() / 3600
+
+    print(f"Latest backup found: {filename}")
+    print(f"Backup is {age_in_hours:.2f} hours old.")
+
+    # 3. Check Threshold and Error
+    if age_in_hours > 25:
+        raise RuntimeError(f"Error: The latest backup is too old! ({age_in_hours:.2f} hours)")
+    else:
+        print("Backup is within the 25-hour limit, continuing checks...")
+
     local_archive_path = os.path.join(DOWNLOAD_DIR, filename)
     output_sql_path = os.path.splitext(local_archive_path)[0]
 
